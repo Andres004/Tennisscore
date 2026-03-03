b2e85ad442991f68baa55bfe4cb74f30e6c8cc4c
unknown <ricardo.andrade@ucb.edu.bo>
2026-03-03 08:34:44 -0400

    refactor:simplificar la logica de advantage y logica usando la diferencia entre los scores

--- src/tennis.js
+++ src/tennis.js
@@ -6,41 +6,28 @@ class Tennis {
 
     score()
     {
-        const scoresNames = ["Love", "15", "30", "40"];
-        let player1Score = scoresNames[this.player1Points] || "40";
-        let player2Score = scoresNames[this.player2Points] || "40";
-
         if (this.player1Points >= 3 && this.player1Points === this.player2Points) 
         {
             return "Deuce";
         }
 
-        if (this.player1Points >= 3 && this.player2Points >= 3) 
-        {
-            if (this.player1Points === this.player2Points + 1) 
-            {
+        if (this.player1Points >= 4 || this.player2Points >= 4) {
+            const diferencia = this.player1Points - this.player2Points;
+
+            if (diferencia === 1) 
                 return "Advantage for Player 1";
-            }
-            if (this.player2Points === this.player1Points + 1) 
-            {
+            if (diferencia === -1) 
                 return "Advantage for Player 2";
-            }
-        }
-        
-        if (this.player1Points >= 4 && this.player1Points >= this.player2Points + 2) {
-            return "Game for Player 1";
-        }
-
-        if (this.player2Points >= 4 && this.player2Points >= this.player1Points + 2) {
-            return "Game for Player 2";
+            if (diferencia >= 2) 
+                return "Game for Player 1";
+            if (diferencia <= -2) 
+                return "Game for Player 2";
         }
 
-         if(this.player1Points == 4){
-            return "Game for Player 1"
-        }
-         if(this.player2Points == 4){
-            return "Game for Player 2"
-        }
+        const scoresNames = ["Love", "15", "30", "40"];
+        let player1Score = scoresNames[this.player1Points] || "40";
+        let player2Score = scoresNames[this.player2Points] || "40";
+
         return player1Score +" - "+ player2Score
     }
 
